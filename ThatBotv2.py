35ead958cd39262386fe3d19bbb243f2bc149397
L. M. R <thatredkite@popo-os.fritz.box>
2020-01-29 18:22:52 +0100

    removed useless junk code

--- ThatBotv2.py
+++ ThatBotv2.py
@@ -3,7 +3,7 @@ import bf
 from discord.ext import commands
 import cogs
 
-version = "0.3.2"
+version = "0.3.3"
 tokens = bf.yamler.Yamler("data/tokens.yml")
 prefix = tokens.load()["prefix"]
 bot = commands.Bot(command_prefix=prefix)
@@ -13,12 +13,5 @@ bot.add_cog(cogs.funstuffcog.FunStuff(bot))
 bot.add_cog(cogs.utilitiescog.Utilities(bot))
 bot.add_cog(cogs.listenercog.Listeners(bot))
 bot.add_cog(cogs.sudocog.Sudostuff(bot))
-
-cmdlist = ""
-for command in bot.commands:
-    commmand.
-    cmdlist += "{0}{1}\n".format(prefix,command)
-    
-print("``` \n{} ```".format(cmdlist))
 
 bot.run(tokens.load()["discordtoken"])
