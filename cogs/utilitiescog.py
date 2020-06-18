b2e0063d437f779de28127d812489f32c30d2d05
ThatRedKite <you@example.com>
2020-06-18 20:03:00 +0200

    added 10 second cooldown for man()

--- cogs/utilitiescog.py
+++ cogs/utilitiescog.py
@@ -2,17 +2,17 @@ from discord.ext import commands
 import discord
 import os, re
 import psutil
-from bf.util import  errormsg
+from bf.util import errormsg
 import logging
 from datetime import datetime
-import  subprocess
-from PIL import Image, ImageColor, ImageDraw
+import subprocess
+from PIL import Image
+
 class utility_commands(commands.Cog):
-    
     def __init__(self, bot:commands.Bot, dirname):
         self.dirname=dirname
-        self.version="b9" 
         self.bot=bot
+        self.version=self.bot.version
         self._last_member=None
 
     @commands.command()
@@ -22,7 +22,7 @@ class utility_commands(commands.Cog):
         """
         process=psutil.Process(os.getpid())
         mem=int(round((process.memory_info()[0] / 1000000)))
-        cpu=process.cpu_percent(interval=1)
+        cpu=process.cpu_percent(interval=10)
         cores_used=len(process.cpu_affinity())   
         cores_total=psutil.cpu_count()
         ping=round(self.bot.latency * 1000, 1)
@@ -43,9 +43,6 @@ class utility_commands(commands.Cog):
 
     @commands.command()
     async def about(self, ctx):
-        """
-            returns: Informations about the Bot
-        """
         embed=discord.Embed(title="about {0}".format(self.bot.user.name), description="{0} is a Discord Bot made By ThatRedKite".format(self.bot.user.name))
         embed.add_field(name="Author:", value="`ThatRedKite#0815`")
         embed.add_field(name="License:", value="`MIT`")
@@ -62,22 +59,6 @@ class utility_commands(commands.Cog):
         embed=discord.Embed(title="This product is licensed under the MIT License",
                                     description="look at https://opensource.org/licenses/MIT if you want to read it")
         embed.color=0x00ffff
-        await ctx.trigger_typing()
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def oldhelp(self, ctx):
-        embed=discord.Embed(title="A list of my commands")
-        commands=""
-        sudocommands=[]
-        for command in self.bot.walk_commands():
-            if not str(command).startswith("yan") and not str(command).startswith("r34"):
-                commands += (str(command) + "\n")
-        embed=discord.Embed(title="A list of my commands:")
-        embed.add_field(name="__Cool Stuff:__", value=commands)
-        embed.set_thumbnail(url=str(self.bot.user.avatar_url))
-        embed.color=0x00ff00
-        embed.set_footer(text="I follow your orders, or do I?")
         await ctx.trigger_typing()
         await ctx.send(embed=embed)
 
@@ -113,6 +94,7 @@ class utility_commands(commands.Cog):
         embed.add_field(name="id", value=f"```fix\n{userid}```")
         await ctx.send(embed=embed)
 
+    @commands.cooldown(1,10,commands.BucketType.user)
     @commands.command()
     async def man(self,ctx,*,args):
         command=["man"]
@@ -133,7 +115,8 @@ class utility_commands(commands.Cog):
                 await ctx.send(embed=embed)
                 
     @commands.command()
-    async def color(self,ctx,*,args):   
+    async def color(self,ctx,*,args):
+        # broken for some reason, need to fix it   
         if len(args.split(" ")) == 1:
             color=int(args[0],16)
             img=Image.new("RGB", (128,128), )
