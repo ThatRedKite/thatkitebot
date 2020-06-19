dca5dcedcb4aef7b432c04b2412b8d17f751bf9f
ThatRedKite <you@example.com>
2020-06-19 15:55:01 +0200

    fixed completely borked code

--- cogs/nsfwcog.py
+++ cogs/nsfwcog.py
@@ -8,9 +8,9 @@ from concurrent.futures import ThreadPoolExecutor
 
 class NSFW(commands.Cog):
     def __init__(self,bot):
-        self.bot = bot
-        self.yanurl = "https://yande.re/post.xml"
-        self.r34url = "https://rule34.xxx/index.php"
+        self.bot=bot
+        self.yanurl="https://yande.re/post.xml"
+        self.r34url="https://rule34.xxx/index.php"
 
     @commands.is_nsfw() # only proceed when in an nsfw channel
     @commands.command()
@@ -20,10 +20,14 @@ class NSFW(commands.Cog):
             payload={ "page": "dapi","tags": tags,"s": "post","q": "index"}
             with ctx.channel.typing():
                 with ThreadPoolExecutor() as executor:
-                    future = executor.submit(url.xml_sequence_parse,self.r34url,"sample_url","pid")
+                    future=executor.submit(url.xml_sequence_parse,
+                                            payload=payload,
+                                            sourceurl=self.r34url,
+                                            attr="sample_url",
+                                            updatevalue="pid")
                 myurl=future.result()
                 embed=discord.Embed(title="Link To picture", url=myurl)
-                embed.color=0xff00cc #  change color to magenta
+                embed.color=0xff00cc
                 embed.set_image(url=myurl) 
                 await ctx.send(embed=embed) 
         else:
@@ -36,41 +40,19 @@ class NSFW(commands.Cog):
             #  only proceed if nsfw is enabled in the bot's settings
             payload={"limit": 100,"tags": tags}
             with ctx.channel.typing():
-                myurl=await url.xml_sequence_parse(payload, self.yanurl, "jpeg_url", "page", False) #  get a sample_url
+                with ThreadPoolExecutor() as executor:
+                    future=executor.submit(url.xml_sequence_parse,
+                                            payload=payload,
+                                            sourceurl=self.yanurl,
+                                            attr="sample_url",
+                                            updatevalue="pid")
+                myurl=future.result()
                 embed=discord.Embed(title="Link To picture", url=myurl)
                 embed.color=0xff00cc
-                #  change color to magenta
                 embed.set_image(url=myurl) 
                 await ctx.send(embed=embed) 
         else:
             await errormsg(ctx,"nsfw content is disabled")
-
-    @commands.is_nsfw() # only proceed when in an nsfw channel
-    @commands.command()
-    async def e621(self, ctx, *,tags):
-        if self.bot.settings[str(ctx.message.guild.id)]["bnsfw"]:
-            #  only proceed if nsfw is enabled in the bot's settings
-            payload={"tags": tags, "limit": 320}
-            await ctx.trigger_typing()
-            with ThreadPoolExecutor() as executor:
-                future = executor.submit(url.monosodiumglutamate,payload,"page")
-            urllist=future.result()
-            with ctx.channel.typing():
-                for x in range(0, 10):
-                    try:
-                        myurl=choice(urllist)
-                        embed=discord.Embed(title="Link To picture", url=myurl)
-                        embed.color=0xff00cc
-                        #  change color to magenta
-                        embed.set_image(url=myurl) 
-                        await ctx.send(embed=embed)
-                    except Exception:
-                        continue
-        else:
-            embed=discord.Embed(title="ERROR!", description="nsfw content is disabled")
-            #  send error message when nsfw is not enabled in the bot's settings
-            embed.color=0xC1121C #  change color to red
-            await ctx.send(embed=embed)
 
     @commands.is_nsfw() # only proceed when in an nsfw channel
     @commands.command()
@@ -81,17 +63,22 @@ class NSFW(commands.Cog):
             #  only proceed if count is below 10, otherwise send an error message
             if count in range(1,20):
                 payload={"limit": 100,"tags": tags}
-                await ctx.trigger_typing()
-                with ThreadPoolExecutor() as executor:
-                    future = executor.submit(url.xml_sequence_parse,payload,self.yanurl, "jpeg_url", "page", True)
-                urllist=future.result()
                 with ctx.channel.typing():
-                    for x in range(0, count):
-                        choice=choice(urllist)
-                        embed=discord.Embed(title="Link To picture", url=choice)
-                        embed.color=0xff00cc#  change color to magenta
-                        embed.set_image(url=choice) 
-                        await ctx.send(embed=embed)                
+                    with ThreadPoolExecutor() as executor:
+                        future=executor.submit(url.xml_sequence_parse,
+                                                payload=payload,
+                                                sourceurl=self.yanurl,
+                                                attr="jpeg_url",
+                                                updatevalue="page",
+                                                islist=True)
+                    urllist=future.result()
+                    with ctx.channel.typing():
+                        for x in range(0, count):
+                            myurl=choice(urllist)
+                            embed=discord.Embed(title="Link To picture", url=myurl)
+                            embed.color=0xff00cc#  change color to magenta
+                            embed.set_image(url=myurl) 
+                            await ctx.send(embed=embed)                
             else:
                 await errormsg(ctx,"please use a number between 1 and 20")
         else:
@@ -105,22 +92,47 @@ class NSFW(commands.Cog):
                 if 1 < count <= 10:
                 #  only proceed if count is below 10
                     payload={ "page": "dapi","tags": tags,"s": "post","q": "index"}
-                    await ctx.trigger_typing()
-                    urllist=await url.xml_sequence_parse(payload, self.r34url, "sample_url", "pid", True)
                     with ctx.channel.typing():
-                        for x in range(0, count):
-                            myurl=choice(urllist) 
-                            embed=discord.Embed(title="Link To picture", url=myurl)
-                            embed.color=0xff00cc#  change color to magenta
-                            embed.set_image(url=myurl) 
-                            await ctx.send(embed=embed)                
+                        with ThreadPoolExecutor() as executor:
+                            future=executor.submit(url.xml_sequence_parse,
+                                                    payload=payload,
+                                                    sourceurl=self.yanurl,
+                                                    attr="jpeg_url",
+                                                    updatevalue="page",
+                                                    islist=True)
+                        urllist=future.result()
+                        with ctx.channel.typing():
+                            for x in range(0, count):
+                                myurl=choice(urllist) 
+                                embed=discord.Embed(title="Link To picture", url=myurl)
+                                embed.color=0xff00cc#  change color to magenta
+                                embed.set_image(url=myurl) 
+                                await ctx.send(embed=embed)                
                 else:
-                    embed=discord.Embed(title="ERROR!", description="enter a number between 1 and 10")
-                    #  send error message when count is too high
-                    embed.color=0xC1121C #  change color to red
-                    await ctx.send(embed=embed)
+                    await errormsg(ctx,"please use a number between 1 and 20")
             else:
-                embed=discord.Embed(title="ERROR!", description="nsfw content is disabled")
-                #  send error message when nsfw is not enabled in the bot's settings
-                embed.color=0xC1121C #  change color to red
-                await ctx.send(embed=embed)
+                await errormsg(ctx,"nsfw content is disabled")
+
+    @commands.is_nsfw() # only proceed when in an nsfw channel
+    @commands.command()
+    async def e621(self, ctx, *,tags):
+        if self.bot.settings[str(ctx.message.guild.id)]["bnsfw"]:
+            #  only proceed if nsfw is enabled in the bot's settings
+            payload={"tags": tags, "limit": 320}
+            await ctx.trigger_typing()
+            with ThreadPoolExecutor() as executor:
+                future=executor.submit(url.monosodiumglutamate,payload,"page")
+            urllist=future.result()
+            with ctx.channel.typing():
+                for x in range(0, 10):
+                    try:
+                        myurl=choice(urllist)
+                        embed=discord.Embed(title="Link To picture", url=myurl)
+                        embed.color=0xff00cc
+                        #  change color to magenta
+                        embed.set_image(url=myurl) 
+                        await ctx.send(embed=embed)
+                    except Exception:
+                        continue
+        else:
+            await errormsg(ctx,"nsfw content is disabled")
