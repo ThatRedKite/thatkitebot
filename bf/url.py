187f48c44ba95ca18873c3405aa68fef26e99cfd
ThatRedKite <you@example.com>
2020-07-31 05:56:19 +0200

    made the url functions async

--- bf/url.py
+++ bf/url.py
@@ -3,13 +3,14 @@ from random import choice
 from bs4 import BeautifulSoup
 import asyncio
 import discord
+import aiohttp
 
-def xml_sequence_parse(payload:dict,sourceurl:str,attr:str,updatevalue:str, islist:bool=False):
+async def xml_sequence_parse(session:aiohttp.ClientSession,payload:dict,sourceurl:str,attr:str,updatevalue:str, islist:bool=False):
     urls=[] # list of gathered urls
     for x in range(0,10): # update the :updatevalue: from 0 to 10
         payload.update({updatevalue:x}) 
-        with requests.get(sourceurl, params=payload) as r:
-            soup=BeautifulSoup(r.text, "lxml") # parse XML
+        async with session.get(sourceurl, params=payload) as r:
+            soup=BeautifulSoup(await r.text(), "lxml") # parse XML
             for post in soup.find_all("post"): # find every :post:
                 sample=dict(post.attrs)[attr] # get every post with the post_attribute :attr:
                 if sample != None: # append sample to :urls: when it is not empty
@@ -26,14 +27,14 @@ def xml_sequence_parse(payload:dict,sourceurl:str,attr:str,updatevalue:str, isli
     if islist is True:
         return urls        # returns the outlist list
 
-def monosodiumglutamate(payload:dict, updatevalue:str):
+async def monosodiumglutamate(session,payload:dict, updatevalue:str):
     urls=[] # initialize the list of URLs 
     url="https://www.e621.net/posts.json" 
-    headers={"User-Agent": "ThatKiteBot/0.9 (from luio950)"} #set user agent because this API is weird
+    headers={"User-Agent": "ThatKiteBot/1.9 (from luio950)"} #set user agent because this API is weird
     for x in range(2): # do that stuff twice
         payload.update({updatevalue:x}) # change the "page"
-        with requests.get(url, headers=headers, params=payload) as r:
-            contentdict=r.json()
+        async with session.get(url, headers=headers, params=payload) as r:
+            contentdict=await r.json()
             for x in contentdict["posts"]:
                 if x != None:
                     urls.append(x["file"]["url"]) # get the file URL and append it to the list
@@ -42,19 +43,19 @@ def monosodiumglutamate(payload:dict, updatevalue:str):
         asyncio.sleep(0.51) # avoid exceeding the rate limit of the API (pretty crude fix, i know)
     return urls
 
-def word(embedmode:bool=True):
-    r=requests.get("https://www.thisworddoesnotexist.com/").text #get the website contents
-    bs=BeautifulSoup(r, "html.parser") 
-    word=bs.find(id="definition-word").string # get the word
-    syllables=bs.find(id="definition-syllables").string # get the syllables
-    definition=bs.find(id="definition-definition").string # get the definition of the word
-    if embedmode:
-        if syllables:
-            embed=discord.Embed(title=word)
-            embed.add_field(name=syllables.lstrip(), value=definition.lstrip())
+async def word(session,embedmode:bool=True):
+    async with session.get("https://www.thisworddoesnotexist.com/") as r: #get the website contents
+        bs=BeautifulSoup(await r.text(), "html.parser") 
+        word=bs.find(id="definition-word").string # get the word
+        syllables=bs.find(id="definition-syllables").string # get the syllables
+        definition=bs.find(id="definition-definition").string # get the definition of the word
+        if embedmode:
+            if syllables:
+                embed=discord.Embed(title=word)
+                embed.add_field(name=syllables.lstrip(), value=definition.lstrip())
+            else:
+                embed=discord.Embed(title=word, description=definition.lstrip())
+            if embed:
+                return embed
         else:
-            embed=discord.Embed(title=word, description=definition.lstrip())
-        if embed:
-            return embed
-    else:
-        return word,definition
+            return word,definition
