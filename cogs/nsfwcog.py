8feb1c5e10c8e527cf740509eee1a10cb932c6c8
ThatRedKite <you@example.com>
2020-08-10 15:22:30 +0200

    optimized API calls

--- cogs/nsfwcog.py
+++ cogs/nsfwcog.py
@@ -1,7 +1,8 @@
+import asyncio
 import typing
 import discord
 from bf import url
-from random import choice
+from random import choice,choices
 from bf.util import errormsg
 from discord.ext import commands
 from concurrent.futures import ThreadPoolExecutor
@@ -9,7 +10,6 @@ from concurrent.futures import ThreadPoolExecutor
 class NSFW(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
-        self.yanurl="https://yande.re/post.xml"
         self.r34url="https://rule34.xxx/index.php"
 
     @commands.is_nsfw() # only proceed when in an nsfw channel
@@ -17,18 +17,12 @@ class NSFW(commands.Cog):
     async def r34(self, ctx, *, tags):
         if self.bot.settings[str(ctx.message.guild.id)]["bnsfw"]:
             #  only proceed if nsfw is enabled in the bot's settings
-            payload={ "page": "dapi","tags": tags,"s": "post","q": "index"}
             with ctx.channel.typing():
-                myurl=url.xml_sequence_parse(session=self.bot.aiohttp_session,
-                                      payload=payload,
-                                      sourceurl=self.r34url,
-                                      attr="sample_url",
-                                      updatevalue="pid")
-
-                embed=discord.Embed(title="Link To picture", url=myurl)
-                embed.color=0xff00cc
-                embed.set_image(url=myurl) 
-                await ctx.send(embed=embed) 
+                myurl=await url.r34url(session=self.bot.aiohttp_session,tags=tags)
+                if len(myurl) == 0:
+                    await errormsg(ctx,"__Nothing Found! Please check your tags and try again!__")
+                else:
+                    await ctx.send(myurl)
         else:
             await errormsg(ctx,"nsfw content is disabled")
 
@@ -37,13 +31,11 @@ class NSFW(commands.Cog):
     async def yan(self, ctx, *, tags):
         if self.bot.settings[str(ctx.message.guild.id)]["bnsfw"]:
             #  only proceed if nsfw is enabled in the bot's settings
-            payload={"limit": 100,"tags": tags}
             with ctx.channel.typing():
-                myurl=await url.xml_sequence_parse(session=self.bot.aiohttp_session,
-                                            payload=payload,
-                                            sourceurl=self.yanurl,
-                                            attr="jpeg_url",
-                                            updatevalue="pid")
+                myurl=await url.yanurlget(
+                                    session=self.bot.aiohttp_session,
+                                    tags=tags
+                                    )
                 embed=discord.Embed(title="Link To picture", url=myurl)
                 embed.color=0xff00cc
                 embed.set_image(url=myurl) 
@@ -53,28 +45,22 @@ class NSFW(commands.Cog):
 
     @commands.is_nsfw() # only proceed when in an nsfw channel
     @commands.command()
-    async def yanspam(self, ctx, count:typing.Optional[int]=10, *, tags):
+    async def yanspam(self,ctx,count:typing.Optional[int]=5,*,tags):
         #  only proceed if nsfw is enabled in the bot's settings
         if self.bot.settings[str(ctx.message.guild.id)]["bnsfw"]:
-            maxlength=self.bot.settings
             #  only proceed if count is below 10, otherwise send an error message
-            if count in range(1,20):
-                payload={"limit": 100,"tags": tags}
+            if count in range(1,11):
                 with ctx.channel.typing():
-                    urllist = await url.xml_sequence_parse(session=self.bot.aiohttp_session,
-                                                payload=payload,
-                                                sourceurl=self.yanurl,
-                                                attr="jpeg_url",
-                                                updatevalue="page",
-                                                islist=True)
-
+                    urllist=await url.yanurlget(
+                                        session=self.bot.aiohttp_session,
+                                        islist=True,
+                                        tags=tags
+                                        )
                     with ctx.channel.typing():
-                        for x in range(0, count):
-                            myurl=choice(urllist)
-                            embed=discord.Embed(title="Link To picture", url=myurl)
-                            embed.color=0xff00cc#  change color to magenta
-                            embed.set_image(url=myurl) 
-                            await ctx.send(embed=embed)                
+                        outlist=set(choices(urllist,k=count+2))
+                        for x in outlist:
+                            await ctx.send(x)
+                            await asyncio.sleep(0.2)         
             else:
                 await errormsg(ctx,"please use a number between 1 and 20")
         else:
@@ -87,23 +73,14 @@ class NSFW(commands.Cog):
                 #  only proceed if nsfw is enabled in the bot's settings
                 if 1 < count <= 10:
                 #  only proceed if count is below 10
-                    payload={ "page": "dapi","tags": tags,"s": "post","q": "index"}
                     with ctx.channel.typing():
-                        with ThreadPoolExecutor() as executor:
-                            future=executor.submit(url.xml_sequence_parse,
-                                                    payload=payload,
-                                                    sourceurl=self.yanurl,
-                                                    attr="jpeg_url",
-                                                    updatevalue="page",
-                                                    islist=True)
-                        urllist=future.result()
-                        with ctx.channel.typing():
-                            for x in range(0, count):
-                                myurl=choice(urllist) 
-                                embed=discord.Embed(title="Link To picture", url=myurl)
-                                embed.color=0xff00cc#  change color to magenta
-                                embed.set_image(url=myurl) 
-                                await ctx.send(embed=embed)                
+                        urllist=await url.r34url(session=self.bot.aiohttp_session,tags=tags,islist=True,count=count)
+                        if urllist ==  ["__Nothing Found! Please check your tags and try again!__"]:
+                            await errormsg(ctx,"__Nothing Found! Please check your tags and try again!__")
+                        else:
+                            for x in urllist:
+                                await ctx.send(x)
+                                await asyncio.sleep(0.2)      
                 else:
                     await errormsg(ctx,"please use a number between 1 and 20")
             else:
