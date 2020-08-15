4e8c41f19411e0bbd0c44ace3f98d4cfa6b3368a
ThatRedKite <you@example.com>
2020-08-15 02:52:16 +0200

    cleaned imports

--- bf/yamler.py
+++ bf/yamler.py
@@ -1,6 +1,28 @@
+"""
+MIT License
+
+Copyright (c) 2020 ThatRedKite
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from pathlib import Path
-import asyncio
-import subprocess
 import json
 import logging
 from os import path
@@ -58,7 +80,7 @@ class Tomler:
             
             raise exc
 
-        except TokenError:
+        except TokenError as exc:
             print(TokenError)
             logging.warning(exc.message)
 
@@ -102,7 +124,6 @@ class Tomler:
 
                 with open(self.path,"wt") as stream:
                     stream.write(datanew)
-
 
 class DataMan:
     def __init__(self):
