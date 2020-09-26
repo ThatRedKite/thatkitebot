833a4af25897f1b8b3dde96356518ce1e244ca2a
ThatRedKite <thatredkite@thatredkite.de>
2020-09-26 16:54:56 +0200

    command to enable/disable debug mode

--- cogs/sudocog.py
+++ cogs/sudocog.py
@@ -1,5 +1,6 @@
 from bf import util
 import discord
+import asyncio
 from discord.ext import commands
 
 class sudo_commands(commands.Cog):
@@ -7,13 +8,9 @@ class sudo_commands(commands.Cog):
         self.bot:commands.Bot=bot
         self.dirname=dirname
 
-    @commands.before_invoke
-    async def pre_fart(self,ctx):
-        print("farts")
-
     @commands.is_owner()
     @commands.command()
-    async def kill(self, ctx):
+    async def kill(self):
         await self.bot.change_presence(status=discord.Status.offline)
         # clear the temp file folder
         util.clear_temp_folder(self.dirname)
@@ -27,20 +24,23 @@ class sudo_commands(commands.Cog):
     @commands.command()
     async def botpurge(self,ctx,count:int=100):
         me=self.bot.user
+        messages=list()
         channel:discord.TextChannel=ctx.channel
         msgcounter=0
         async for message in channel.history(limit=5000).filter(lambda x: x.author == me):
             msgcounter += 1
             if msgcounter < count:
                 await message.delete()
-                print(msgcounter)
             else:
                 break
-
-    @pre_fart
-    @commands.command(pass_context=True)
-    async def tester(self,ctx):
-        await ctx.send("tester")
+    
+    @commands.is_owner()
+    @commands.command()
+    async def debug(self,ctx,state:str):
+        self.bot.debugmode=util.bool_parse(state)
+        msg=await ctx.send(f"set debug to {self.bot.debugmode}")
+        await asyncio.sleep(2)
+        await msg.delete()
 
             
 
