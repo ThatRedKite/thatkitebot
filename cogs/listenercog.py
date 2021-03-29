f231d84799930f06a83d2e04f0d3f8c7143c3b75
ThatRedKite <thatredkite@thatredkite.de>
2021-03-29 17:33:32 +0200

    removed annoying feature preventing @everyone

--- cogs/listenercog.py
+++ cogs/listenercog.py
@@ -48,30 +48,10 @@ class ListenerCog(commands.Cog):
         else:
             raise error
 
-    @tasks.loop(minutes=5.0)
-    async def statuschange(self):
-        ontime = datetime.now() - self.bot.starttime
-        times = str(ontime).split(".")
-
-        possible_status = [
-            (Status.idle, "nothing"),
-            (Status.online, "with code"),
-            (Status.online, f"{self.bot.command_prefix}help"),
-            (Status.online, f"uptime:    {times[0]}"),
-            (Status.dnd, "with trains"),
-            (Status.dnd, "with myself"),
-            (Status.online, "games"),
-            (Status.online, "dead"),
-            (Status.online, "K.I.T.E The Game")
-        ]
-
-        chosen_status, chosen_message = random.choice(possible_status)
-        await self.bot.change_presence(status=chosen_status, activity=discord.Game(chosen_message))
-
     @tasks.loop(hours=1.0)
     async def reset_invoke_counter(self):
         self.bot.command_invokes_hour = 0
-
+    """
     @commands.Cog.listener()
     @commands.bot_has_guild_permissions(ban_members=True)
     async def on_message(self, message: discord.Message):
@@ -92,12 +72,11 @@ class ListenerCog(commands.Cog):
             elif allowed_role not in message.author.roles and self.strikes.get(member) == 2:
                 await author.send("Okay you didn't listen. You have been banned.")
                 #await guild.ban(member, reason="mentioning everyone without permission")
+    """
 
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"\nbot successfully started!")
-        await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Game("booting"))
-        self.statuschange.start()
         self.reset_invoke_counter.start()
 
     @commands.Cog.listener()
