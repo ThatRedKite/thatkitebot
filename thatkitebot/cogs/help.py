19769cb4b0aeced3389b075f416b4a228c3538f6
ThatRedKite <thatredkite@thatredkite.de>
2021-10-26 14:19:57 +0200

    update repo URL

--- thatkitebot/cogs/help.py
+++ thatkitebot/cogs/help.py
@@ -2,7 +2,8 @@
 #  MIT License
 #
 #  Copyright (c) 2021 dunnousername
-#
+#  Copyright (c) 2021 ThatRedKite
+
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 #  documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
 #  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
@@ -23,7 +24,7 @@ from discord.ext import commands
 import random
 
 info = {
-    'repo': 'https://github.com/ThatRedKite/ThatKiteBotv2',
+    'repo': 'https://github.com/ThatRedKite/thatkitebot',
     'name': 'ThatKiteBot'
 }
 
