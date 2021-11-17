08aa6eedcfd8293fdbe8aca02e69c2a9bee5cc70
ThatRedKite <thatredkite@thatredkite.de>
2021-11-17 16:13:22 +0100

    delete old settings code

--- thatkitebot/backend/__init__.py
+++ thatkitebot/backend/__init__.py
@@ -17,8 +17,6 @@
 #  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 # ------------------------------------------------------------------------------
-
-from . import settings
 from . import url
 from . import util
 from . import misc
