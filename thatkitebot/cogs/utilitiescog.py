011eef8b1325fc3a2d41ce2d05f04be8fc2f9ff3
ThatRedKite <thatredkite@thatredkite.de>
2021-12-01 20:41:07 +0100

    remove unused code

--- thatkitebot/cogs/utilitiescog.py
+++ thatkitebot/cogs/utilitiescog.py
@@ -18,7 +18,6 @@
 #  SOFTWARE.
 # ------------------------------------------------------------------------------
 
-
 import discord
 from discord.ext import commands
 from thatkitebot.backend import misc as back
@@ -29,20 +28,7 @@ class UtilityCommands(commands.Cog, name="utility commands"):
     def __init__(self, bot: commands.Bot):
         self.dirname = bot.dirname
         self.bot = bot
-    """
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def help(self, ctx):
-        embed = discord.Embed(title="**a list of the bot's commands**")
-        for cog in self.bot.cogs:
-            commandstring = ""
-            for command in self.bot.get_cog(cog).walk_commands():
-                if cog != "NSFW":commandstring += f"{command}\n"
-            if len(commandstring) > 1:
-                embed.add_field(name=f"**{cog}**", value=f"\n{commandstring}", inline=True)
-        embed.set_footer(text=f"\nThatKiteBot² version {self.bot.version}", icon_url=self.bot.user.avatar_url)
-        await ctx.send(embed=embed)
-    """
+
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command(pass_context=True, aliases=["uptime", "load"])
     async def status(self, ctx):
@@ -70,7 +56,7 @@ class UtilityCommands(commands.Cog, name="utility commands"):
                                     commands invoked this hour: **{self.bot.command_invokes_hour}**
                                     """, inline=False)
 
-            embed.set_thumbnail(url=str(self.bot.user.avatar_url))
+            embed.set_thumbnail(url=str(self.bot.user.avatar.url))
 
             if not self.bot.debugmode:
                 if cpu >= 90.0:
