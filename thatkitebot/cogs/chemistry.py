5063b685f3158b356acd21d5fb0777f5057b3505
DDL <dimin21590@gmail.com>
2021-12-02 15:34:31 +0200

    fixed checmistry mm command

--- thatkitebot/cogs/chemistry.py
+++ thatkitebot/cogs/chemistry.py
@@ -32,10 +32,10 @@ class ChemCog(commands.Cog, name="Chemistry commands"):
     async def molar_mass(self, ctx, *, formula):
         try:
             masspattern = re.compile("mass: (\S+)\n")
-            mass_analyzed = molmass.analyze(formula.upper())
+            mass_analyzed = molmass.analyze(formula)
             avg, mono, nom, mean = re.findall(masspattern, mass_analyzed)
             embed = discord.Embed(
-                title=f"Molar mass for {formula.upper()} (in g/mol)",
+                title=f"Molar mass for {formula} (in g/mol)",
                 description=f"Average mass: `{avg}`\nMonoisotopic mass: `{mono}`\nNominal mass: `{nom}`\nMean mass: `{mean}`")
             embed.color = util.EmbedColors.lime_green
             await ctx.send(embed=embed)
