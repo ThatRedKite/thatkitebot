7f0341174c13aedbbfd7472df0abd3f5023236da
ThatRedKite <thatredkite@thatredkite.de>
2021-12-05 15:35:07 +0100

    add db memory usage to status command

--- thatkitebot/cogs/utilitiescog.py
+++ thatkitebot/cogs/utilitiescog.py
@@ -20,50 +20,60 @@
 
 import discord
 from discord.ext import commands
-from thatkitebot.backend import misc as back
 from thatkitebot.backend.util import EmbedColors as ec
+import psutil
+from datetime import datetime
+import si_prefix
 
 
 class UtilityCommands(commands.Cog, name="utility commands"):
     def __init__(self, bot: commands.Bot):
         self.dirname = bot.dirname
+        self.redis = bot.redis
         self.bot = bot
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command(pass_context=True, aliases=["uptime", "load"])
     async def status(self, ctx):
         """
-        Bot's Current Status
+        Displays the status of the bot.
         """
-        async with ctx.typing():
-            mem, cpu, cores_used, cores_total, ping, uptime = await back.get_status(self.bot.pid, self.bot)
-            total_users = sum([users.member_count for users in self.bot.guilds])
-            guilds = len(self.bot.guilds)
-            embed = discord.Embed()
-            embed.add_field(name="System status",
-                            value=f"""RAM usage: **{mem} Mb**
-                                    CPU usage: **{cpu} %**
-                                    core affinity: **{cores_used}/{cores_total}**
-                                    uptime: **{uptime}**
-                                    ping: **{ping} ms**""")
+        process = psutil.Process(self.bot.pid)
+        mem = si_prefix.si_format(process.memory_info()[0])
+        redismem = si_prefix.si_format((await self.redis.info())["used_memory"])
+        cpu = psutil.cpu_percent(interval=None)
+        ping = round(self.bot.latency * 1000, 1)
+        uptime = str(datetime.now() - self.bot.starttime).split(".")[0]
+        total_users = sum([users.member_count for users in self.bot.guilds])
+        guilds = len(self.bot.guilds)
 
-            embed.add_field(name="Bot stats",
-                            value=f"""guilds: **{guilds}**
-                                    extensions loaded: **{len(self.bot.extensions)}**
-                                    total users: **{total_users}**
-                                    bot version: **{self.bot.version}**
-                                    total command invokes: **{self.bot.command_invokes_total}**
-                                    commands invoked this hour: **{self.bot.command_invokes_hour}**
-                                    """, inline=False)
+        embed = discord.Embed()
+        embed.add_field(name="System status",
+                        value=f"""RAM usage: **{mem}B**
+                                DB memory usage: ** {redismem}B**
+                                CPU usage: **{cpu} %**
+                                uptime: **{uptime}**
+                                ping: **{ping} ms**""")
 
-            embed.set_thumbnail(url=str(self.bot.user.avatar.url))
+        embed.add_field(name="Bot stats",
+                        value=f"""guilds: **{guilds}**
+                                extensions loaded: **{len(self.bot.extensions)}**
+                                total users: **{total_users}**
+                                bot version: **{self.bot.version}**
+                                total command invokes: **{self.bot.command_invokes_total}**
+                                commands invoked this hour: **{self.bot.command_invokes_hour}**
+                                """, inline=False)
 
-            if not self.bot.debugmode:
-                if cpu >= 90.0:
-                    embed.color = ec.traffic_red
-                    embed.set_footer(text="Warning: CPU usage over 90%")
-                else: embed.color = ec.lime_green
-            else:embed.color = ec.purple_violet
+        embed.set_thumbnail(url=str(self.bot.user.avatar.url))
+
+        if not self.bot.debugmode:
+            if cpu >= 90.0:
+                embed.color = ec.traffic_red
+                embed.set_footer(text="Warning: CPU usage over 90%")
+            else:
+                embed.color = ec.lime_green
+        else:
+            embed.color = ec.purple_violet
         await ctx.send(embed=embed)
 
     # TODO: make this command actually do something
