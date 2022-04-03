aa8ebce81fe986e6419b920e8bac17802c69994c
thatredkite <thatredkite@thatredkite.de>
2022-04-03 19:56:01 +0200

    cope pilot

--- thatkitebot/backend/cache.py
+++ thatkitebot/backend/cache.py
@@ -2,7 +2,6 @@
 
 from discord import Message
 from aioredis import Redis
-from datetime import timedelta
 import base64
 
 
@@ -10,10 +9,11 @@ async def add_message_to_cache(redis: Redis, message: Message):
     key = f"{message.guild.id}:{message.channel.id}:{message.author.id}:{message.id}"
     msgdict = dict(
         content=base64.b64encode(message.clean_content.encode("UTF-8")),
-        created_at=hex(int(message.created_at.timestamp()))
+        created_at=int(message.created_at.timestamp())
     )
     await redis.hmset(key, msgdict)
-    await redis.expire(key, timedelta(weeks=1))
+    # set the expiration time of the message in the cache to 1 week
+    await redis.expire(key, 604800)
     return message
 
 
@@ -22,5 +22,7 @@ async def get_contents(redis: Redis, gid, cid, uid):
     a = list()
     async for mkey in redis.scan_iter(key):
         c = await redis.hget(mkey, "content")
+        # decode the content from base64 to UTF-8 and add it to the list a
         a.append(base64.b64decode(c).decode("UTF-8"))
     return a
+
