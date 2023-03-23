e67f951a38b06c07826f4bbb7c1c9f9e0ecd1024
thatredkite <thatredkite@thatredkite.de>
2023-03-23 19:55:28 +0100

    refactor

--- src/pcb_mod/setup.py
+++ src/pcb_mod/setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup, Extension
 
-module_pcb = Extension("pcb_mod", sources = ["pcb_calc.c"])
+module_pcb = Extension("pcb_mod", sources=["pcb_calc.c"])
 
-setup(name = "PCB_Calc",
-      version = "1.1",
-      description = "C library for calculating PCB and electronics related topics",
-      ext_modules = [module_pcb])
+setup(name="PCB_Calc",
+      version="1.1",
+      description="C library for calculating PCB and electronics related topics",
+      ext_modules=[module_pcb]
+      )
