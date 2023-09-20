af719f0d85ed52f869ccceede91e4e10c128cdc6
thatredkite <thatredkite@thatredkite.de>
2023-09-20 21:40:31 +0200

    small uwu rewrite

--- thatkitebot/cogs/uwucog.py
+++ thatkitebot/cogs/uwucog.py
@@ -30,58 +30,37 @@ class UwuCog(commands.Cog, name="UwU Commands"):
     async def _uwu_enabled(self, ctx):
         return await RedisFlags.get_guild_flag(self.redis, ctx.guild.id, RedisFlags.UWU)
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        # Check if the user is a bot
+    async def _listener_checks(self, message):
         if message.author.bot:
-            return
+            return False
 
         if not await self._uwu_enabled(message):
-            return
+            return False
 
-        # Check if the message is in an UwU channel
-        if not await self.redis.sismember(f"uwu_channels:{message.guild.id}", message.channel.id) and not await self.redis.sismember(f"uwu_users:{message.guild.id}", message.author.id):
-            return
+        # check if message is in an uwu channel
+        is_channel = await self.redis.sismember(f"uwu_channels:{message.guild.id}", str(message.channel.id))
+        is_user = await self.redis.sismember(f"uwu_users:{message.guild.id}", str(message.author.id))
+        if not (is_user and is_channel):
+            return False
 
-        # Carter's code (Updated)
-        if not message.webhook_id:
-            await self.bot.process_commands(message)
-            webhooks = await message.channel.webhooks()
-            webhook: discord.Webhook = next((hook for hook in webhooks if hook.name == "uwuhook"), None)
-            if not webhook:
-                webhook = await message.channel.create_webhook(name='uwuhook', reason='uwuhook is for uwu channel uwu')
+        return True
 
-            files = []
-            for attachment in message.attachments:
-                async with self.bot.aiohttp_session.get(attachment.url) as resp:
-                    fp = io.BytesIO(await resp.read())
-                    files.append(discord.File(fp, filename=attachment.filename))
-            await message.delete()
-            # convert the input string to ascii
-            msg_len = len(message.content) + 20
-            msg = unidecode(message.content)
+    #
+    #   --- UwU command group
+    #
 
-            # if the user cant embed links, make links not embed by surrounding them with <>
-            if not message.channel.permissions_for(message.author).embed_links:
-                links = r"(https?:\/\/[A-Za-z0-9\-._~!$&'()*+,;=:@\/?]+)"
-                msg = re.sub(links, r"<\1>" , msg)
+    uwu = discord.SlashCommandGroup(
+        "uwu",
+        "UwUify Commands",
+        checks=[pc.mods_can_change_settings, lambda ctx: ctx.guild is not None]
+    )
 
-            msg_small = textwrap.wrap(msg, msg_len)
-            msg = await uwuify(msg_small[0], message.id)
-            # split it up while maintaining whole words
-            output = textwrap.wrap(msg, 2000)
-            # for each new "message" send it in the channel
-            # thanks paradox for breaking the >2000 msg limit
-            await webhook.send(content=output[0],
-                               username=message.author.name + "#" + message.author.discriminator,
-                               avatar_url=message.author.display_avatar.url,
-                               files=files)
-
-    # Sorry mom
-    @bridge.bridge_command(name="uwu_channel", aliases=["uwuchannel", "uwuch"],
-                           description="Make a channel automatically UwU every message",
-                           checks=[commands.check(pc.mods_can_change_settings).predicate])
-    async def add_uwu_channel(self, ctx: commands.Context, channel: discord.TextChannel):
+    @uwu.command(
+        name="channel",
+        description="Make a channel automatically UwU every message",
+        checks=[pc.mods_can_change_settings]
+    )
+    async def _add_channel(self, ctx: discord.ApplicationContext, channel: discord.TextChannel):
         """
         uwuifies an entire channel by deleting the original messages
         and replacing them with bot clones.
@@ -91,28 +70,31 @@ class UwuCog(commands.Cog, name="UwU Commands"):
 
         Only administrators and moderators can use this command.
         """
+        await ctx.defer()
         if not await self._uwu_enabled(ctx):
-            return await ctx.respond("This command is disabled on this server (setting `UWU` is set `FALSE`)")
+            return await ctx.followup.send("This command is disabled on this server.")
 
         key = f"uwu_channels:{ctx.guild.id}"
-        if not await self.redis.sismember(key, channel.id):
+        if not await self.redis.sismember(key, str(channel.id)):
             await self.redis.sadd(key, channel.id)
-            await ctx.respond(f"{channel.mention} is now an UwU channel.")
+            await ctx.followup.send(f"{channel.mention} is now an UwU channel.")
             print(f"{ctx.guild.name}:  {ctx.author.name}#{ctx.author.discriminator} uwuified #{channel.name}")
         else:
             try:
                 await self.redis.srem(key, channel.id)
             except aioredis.ResponseError:
-                await ctx.respond(f"{channel.mention} is not an UwU channel.")
+                await ctx.followup.send(f"{channel.mention} is not an UwU channel.")
                 return
 
             await ctx.respond(f"{channel.mention} is no longer an UwU channel.")
             print(f"{ctx.guild.name}:  {ctx.author.name}#{ctx.author.discriminator} de-uwuified #{channel.name}")
 
-    @bridge.bridge_command(name="uwu_user", aliases=["fuck_you"], hidden=True,
-                           description="Make a user automatically UwU every message",
-                           checks=[commands.check(pc.mods_can_change_settings).predicate])
-    async def add_uwu_user(self, ctx: bridge.BridgeContext, user: discord.User):
+    @uwu.command(name="user", description="Turn every message from this user into unintelligible uwu gibberish.")
+    async def add_user(
+            self,
+            ctx: discord.ApplicationContext,
+            user: discord.Option(discord.User, description="The user to uwuify.", required=True)
+    ):
         """
         uwuifies all messages sent by a specific person by deleting
         their original messages and replacing them with a bot clone.
@@ -120,24 +102,92 @@ class UwuCog(commands.Cog, name="UwU Commands"):
         Usage:
         `+uwu_user @user True` - toggle the setting for a user
 
-        Only admins can use this command.
+        Only admins and moderators can use this command.
         """
+        await ctx.defer()
         if not await self._uwu_enabled(ctx):
-            return await ctx.respond("This command is disabled on this server (setting `UWU` is set `FALSE`)")
+            return await ctx.followup.send("This command is disabled on this server.")
 
         key = f"uwu_users:{ctx.guild.id}"
         if not await self.redis.sismember(key, user.id):
             await self.redis.sadd(key, user.id)
-            await ctx.respond(f"{user.name} is now fucked.")
-            print(f"{ctx.guild.name}: {ctx.author.name}#{ctx.author.discriminator} uwuified {user.name}#{user.discriminator}")
+            await ctx.followup.send(f"{user.name} is now fucked.")
+            print(
+                f"{ctx.guild.name}: {ctx.author.name}#{ctx.author.discriminator} uwuified {user.name}#{user.discriminator}")
         else:
             try:
                 await self.redis.srem(key, user.id)
             except aioredis.ResponseError:
-                await ctx.respond(f"{user.name} is not fucked.")
+                await ctx.followup.send(f"{user.name} is not fucked.")
                 return
-            await ctx.respond(f"{user.name} is now unfucked.")
-            print(f"{ctx.guild.name}: {ctx.author.name}#{ctx.author.discriminator} de-uwuified {user.name}#{user.discriminator}")
+            await ctx.followup.send(f"{user.name} is now unfucked.")
+            print(
+                f"{ctx.guild.name}: {ctx.author.name}#{ctx.author.discriminator} de-uwuified {user.name}#{user.discriminator}")
+
+    #
+    #   --- main listener function ---
+    #
+
+    @commands.command(name="uwu_user")
+    async def _uwu_user(self, ctx):
+        await ctx.send("This command is deprecated, please use the slash command version")
+
+    @commands.command(name="uwu_channel")
+    async def _uwu_channel(self, ctx):
+        await ctx.send("This command is deprecated, please use the slash command version")
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        # Check if the user is a bot
+        if not await self._listener_checks(message):
+            return
+
+        print("UwU")
+
+        # Carter's code (Updated)
+        if not message.webhook_id:
+            await self.bot.process_commands(message)
+            webhooks = await message.channel.webhooks()
+            webhook: discord.Webhook = next((hook for hook in webhooks if hook.name == "uwuhook"), None)
+            if not webhook:
+                try:
+                    webhook = await message.channel.create_webhook(
+                        name='uwuhook',
+                        reason='uwuhook is for UwU'
+                    )
+                except discord.HTTPException:
+                    print("Fuck")
+                    return
+
+            files = []
+            for attachment in message.attachments:
+                async with self.bot.aiohttp_session.get(attachment.url) as resp:
+                    fp = io.BytesIO(await resp.read())
+                    files.append(discord.File(fp, filename=attachment.filename))
+            await message.delete(reason="UwU Delete")
+            # convert the input string to ascii
+            msg_len = len(message.content) + 20
+            msg = unidecode(message.content)
+
+            # if the user cant embed links, make links not embed by surrounding them with <>
+            if not message.channel.permissions_for(message.guild.get_member(message.author.id)).embed_links:
+                links = r"(https?:\/\/[A-Za-z0-9\-._~!$&'()*+,;=:@\/?]+)"
+                msg = re.sub(links, r"<\1>", msg)
+
+            msg_small = textwrap.wrap(msg, msg_len)
+            msg = await uwuify(msg_small[0], message.id)
+            # split it up while maintaining whole words
+            output = textwrap.wrap(msg, 2000)
+            # for each new "message" send it in the channel
+            # thanks paradox for breaking the >2000 msg limit
+            await webhook.send(
+                content=output[0],
+                username=message.author.name + "#" + message.author.discriminator,
+                avatar_url=message.author.display_avatar.url,
+                files=files
+            )
+
+
 
 
 def setup(bot):
