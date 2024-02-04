d6a133e654efb5fa4d337c453e67cd92dcd00b5e
ThatRedKite <thatredkite@thatredkite.de>
2024-02-04 17:16:22 +0100

    change formatting

--- src/pcb_mod/setup.py
+++ src/pcb_mod/setup.py
@@ -4,8 +4,9 @@ from distutils.core import setup, Extension
 
 module_pcb = Extension("pcb_mod", sources=["pcb_calc.c"])
 
-setup(name="PCB_Calc",
-      version="1.1",
-      description="C library for calculating PCB and electronics related topics",
-      ext_modules=[module_pcb]
-      )
+setup(
+    name="PCB_Calc",
+    version="1.1",
+    description="C library for calculating PCB and electronics related topics",
+    ext_modules=[module_pcb]
+)
