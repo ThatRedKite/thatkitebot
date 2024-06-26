5938a9cd91401268385c32a36347d8c9afee5195
Caraffa-git <caraffa445@gmail.com>
2024-06-26 23:22:14 +0200

    settings - add, edit, remove fields and factory reset

--- thatkitebot/cogs/info.py
+++ thatkitebot/cogs/info.py
@@ -3,6 +3,7 @@ import re
 import json
 import random
 from pathlib import Path
+from typing import Literal
 
 import aiofiles
 import discord_emoji
@@ -145,6 +146,155 @@ class NavigationView(View):
 
         await interaction.response.edit_message(embed=embed, content=None, view = self)
 
+class EditFieldsView(View):
+    def __init__(self, bot, config, config_file, id ,action):
+        super().__init__(timeout = None)
+
+        self.bot = bot
+        self.config = config 
+        self.config_file = config_file
+
+        self.add_dropdown(config_file, id, action)
+
+    @staticmethod
+    async def create(bot, guild, config: Config, id, action: Literal["edit", "remove"]):
+        return EditFieldsView(bot, config, await config.get(guild), id, action)
+
+    def add_dropdown(self, config_file, id, action):
+        """Add dropdown with options."""
+        options = []
+        for field_id, section in enumerate(config_file [id]["fields"]):
+            options.append(discord.SelectOption(label=f'{id + 1}.{field_id + 1}. {section["name"]}'))
+
+        select = Select(
+            placeholder='Select a field...', 
+            min_values=1, 
+            max_values=1, 
+            options=options
+        )
+
+        if(action == "remove"):
+            select.callback = self.remove_field_callback
+        if(action == "edit"):
+            select.callback = self.edit_field_callback
+
+        self.add_item(select)
+
+    async def edit_field_callback(self, interaction: discord.Interaction):
+        # get id and field id
+        id = int(interaction.data["values"][0].split(".")[0]) - 1
+        field_id = int(interaction.data["values"][0].split(".")[1]) - 1
+
+        modal = FieldModal(title=f"Field editor :pencil:",
+                           config_file=await self.config.get(interaction.guild), section_id=id, field_id=field_id,
+                           bot=self.bot)
+
+        await interaction.response.send_modal(modal)
+
+    async def remove_field_callback(self, interaction: discord.Interaction):
+        # get id and field id
+        id = int(interaction.data["values"][0].split(".")[0]) - 1
+        field_id = int(interaction.data["values"][0].split(".")[1]) - 1
+
+        config_file = await self.config.get(interaction.guild)
+        name = config_file[id]["title"]
+        emoji = config_file[id]["emoji"]
+
+        del config_file[id]["fields"][field_id]
+
+        await self.config.update(interaction.guild, config_file)
+        await interaction.response.edit_message(
+            content=f"Field no. {field_id + 1} has been removed from section {name} {emoji}.", view=None)
+
+class FieldModal(discord.ui.Modal):
+    def __init__(self, bot, config_file, section_id, field_id, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.bot = bot
+        self.config_file = config_file
+        self.section_id = section_id
+        self.field_id = field_id
+
+        name = config_file[section_id]["fields"][field_id]["name"]
+        contents = config_file[section_id]["fields"][field_id]["value"]
+        inline = config_file[section_id]["fields"][field_id]["inline"]
+
+        _title = discord.ui.InputText(
+            label="Title",
+            value=name if name != "" else "Example title 💡",
+            placeholder="Think of an interesting title",
+            max_length=256
+        )
+
+        _contents = discord.ui.InputText(
+            label="Contents",
+            value=contents if contents != "" else "[Example hyperlink](https://github.com/ThatRedKite/thatkitebot)",
+            placeholder="Here you can type the contents of this field in your embed",
+            style=discord.InputTextStyle.long,
+            max_length=1024
+        )
+
+        _inline = discord.ui.InputText(
+            label="In-line",
+            value=inline if inline != "" else "True",
+            placeholder="True or False",
+            max_length=5
+        )
+
+        self.add_item(_title)
+        self.add_item(_contents)
+        self.add_item(_inline)
+
+    async def callback(self, interaction: discord.Interaction):
+
+        self.config_file[self.section_id]["fields"][self.field_id]["name"] = self.children[0].value  # set title of the field
+        self.config_file[self.section_id]["fields"][self.field_id]["value"] = self.children[1].value  # set value of the field
+
+        # check if "inline" input value is correct
+        if str(self.children[2].value).lower() == "true":
+            self.config_file[self.section_id]["fields"][self.field_id]["inline"] = True
+        elif str(self.children[2].value).lower() == "false":
+            self.config_file[self.section_id]["fields"][self.field_id]["inline"] = False
+        else:
+            await interaction.response.send_message("Invalid `in-line` value!", ephemeral=True)
+            return
+
+        # update config
+        await self.bot.get_cog("InfoCog").config.update(interaction.guild, self.config_file)
+
+        await interaction.response.send_message(
+            f"Field with ID {self.field_id + 1} in section {self.config_file[self.section_id]['title']} has been changed.")
+        
+
+        
+class FactoryResetModal(discord.ui.Modal):
+    def __init__(self, config, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.config = config
+
+        _decision = discord.ui.InputText(
+            label="Are you sure? Type \"YES\" or \"NO\"",
+            value="",
+            placeholder="It will **replace all of your sections!",
+            max_length=256
+        )
+
+        self.add_item(_decision)
+
+    async def callback(self, interaction: discord.Interaction):
+        """Factory reset all the sections in /info """
+
+        if self.children[0].value == "YES":
+             # load default config
+            default_config = await self.config.get_default()
+
+            # replace the contents of the file with the default values
+            await self.config.update(interaction.guild, default_config)
+
+            await interaction.response.send_message(content="Default settings have been restored.", view=None)
+        else:
+            await interaction.response.send_message(content="Nothing has changed.", view=None)
 
 
 class InfoCog(commands.Cog):
@@ -253,6 +403,64 @@ class InfoCog(commands.Cog):
 
         await self.config.update(ctx.guild, config_file)
         await ctx.respond(f"Section {name} {emoji} has been removed.")
+
+    @info_settings.command(name="edit-field")
+    async def edit_field(self, ctx: discord.ApplicationContext,
+                         section: Option(str, "Pick a section!", required=True, autocomplete=Config.get_sections),
+                         option: Option(str, "Choose option.", required=True, choices=["edit", "remove", "add"])):
+        """Add, edit or remove a field in section"""
+
+        config_file = await self.config.get(ctx.guild)
+
+        empty_field = {
+            "name": "",
+            "value": "",
+            "inline": True
+        }
+
+        # check if "option" input is valid
+        if option not in {"edit", "remove", "add"}:
+            await ctx.respond("Incorrect option!", ephemeral=True)
+            return
+
+        # check if given section name has valid id
+        id = await Utility.retrive_section_id(section, len(config_file))
+        if id < 0:
+            await ctx.respond("Incorrect section name!", ephemeral=True)
+            return
+
+        # add empty field to config
+        if option == "add":
+            config_file[id]["fields"].append(empty_field)
+            new_field_id = len(config_file[id]["fields"]) - 1
+
+            # prepare modal for user to edit field
+            await self.config.update(ctx.guild, config_file)
+            config_file = await self.config.get(ctx.guild)
+            modal = FieldModal(title=f"Editing new field {discord_emoji.to_uni(":pencil:")}",
+                               config_file=config_file, section_id=id,
+                               field_id= new_field_id, bot=self.bot)
+
+            await ctx.send_modal(modal)
+            return
+
+        # check if there are any fields to edit/remove
+        if len(config_file[id]["fields"]) == 0:
+            await ctx.respond("There is no fields to edit/remove!")
+            return
+
+        # prepare dropdown list of existing fields to edit/remove
+        view = await EditFieldsView.create(self.bot, ctx.guild, self.config, id, option)
+
+        await ctx.respond(view=view)
+
+# TODO edit_section edit_footer (merge it in edit_section)
+
+    @info_settings.command(name="factory-reset")
+    async def factory_reset(self, ctx):
+        modal = FactoryResetModal(title="Factory Reset!", config=self.config)
+        await ctx.send_modal(modal)
+
 
     # TODO it's just temporary debug function    
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
