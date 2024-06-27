89eea1780ed587752422040f8e4a55df9f53df1d
Caraffa-git <caraffa445@gmail.com>
2024-06-27 11:04:01 +0200

    refactor

--- thatkitebot/cogs/info.py
+++ thatkitebot/cogs/info.py
@@ -59,244 +59,6 @@ class Config:
         return [f"{id + 1}. {section['title']}" for id, section in enumerate(config) if section['title'].lower().startswith(ctx.value.lower())]
 
 
-class NavigationView(View):
-    def __init__(self, config, config_file, buttons, dropdown, toggle):
-        super().__init__(timeout = None)
-
-        self.config = config 
-        self.config_file = config_file
-
-        # Current states of the view contents
-        self.toggle = toggle
-        self.state_buttons = buttons
-        self.state_dropdown = dropdown
-        self.current_embed_id = None
-
-        if dropdown and not toggle:
-            self.add_dropdown(config_file)
-        if buttons and not toggle:
-            self.add_buttons()
-
-    @staticmethod
-    async def create(guild, config: Config = None, buttons: bool=False, dropdown: bool= True, toggle: bool = False):
-        return NavigationView(config, await config.get(guild), buttons, dropdown, toggle)
-
-    def add_buttons(self):
-        """Add navigation buttons."""
-        prev = Button(emoji="⬅️", style=discord.ButtonStyle.gray, custom_id="prev")
-        next = Button(emoji="➡️", style=discord.ButtonStyle.gray, custom_id="next")
-
-        prev.callback = self.button_callback
-        next.callback = self.button_callback
-
-        self.add_item(prev)
-        self.add_item(next)
-
-    def add_dropdown(self, config_file):
-        """Add dropdown with options."""
-        options = []
-        for id, section in enumerate(config_file):
-            if discord_emoji.to_uni(section["emoji"]):
-                options.append(discord.SelectOption(label=f'{id + 1}. {section["title"]}', emoji=discord_emoji.to_uni(section["emoji"]), value = f"{id}"))
-            else:
-                options.append(discord.SelectOption(label=f'{id + 1}. {section["title"]}', emoji=section["emoji"], value = f"{id}"))
-
-        select = Select(
-            placeholder='Select a section...', 
-            min_values=1, 
-            max_values=1, 
-            options=options
-        )
-
-        select.callback = self.dropdown_callback
-
-        self.add_item(select)
-
-    async def button_callback(self, interaction: discord.Interaction):
-        """Callback function for the next button."""
-        max_id = await self.config.size(interaction.guild)
-
-        if interaction.custom_id == "next":
-            self.current_embed_id = (self.current_embed_id + 1) % max_id
-        else:
-            self.current_embed_id = (self.current_embed_id - 1) % max_id
-
-        config_file = await self.config.get(interaction.guild)
-        embed = await get_embed(self.current_embed_id, config_file)
-
-        await interaction.response.edit_message(embed=embed, content=None, view = self)
-
-
-    async def dropdown_callback(self, interaction: discord.Interaction):
-        """Callback function for the dropdown selection."""
-        self.current_embed_id = int(interaction.data["values"][0])
-        config_file = await self.config.get(interaction.guild)
-
-        embed = await get_embed(self.current_embed_id, config_file)
-
-        if(self.toggle):
-            self.disable_all_items
-        else:
-            if(not self.state_buttons): 
-                self.add_buttons()
-                self.state_buttons = True
-            if(not self.state_dropdown): 
-                self.add_dropdown(config_file)
-                self.state_dropdown =  True
-
-        await interaction.response.edit_message(embed=embed, content=None, view = self)
-
-class EditFieldsView(View):
-    def __init__(self, bot, config, config_file, id ,action):
-        super().__init__(timeout = None)
-
-        self.bot = bot
-        self.config = config 
-        self.config_file = config_file
-
-        self.add_dropdown(config_file, id, action)
-
-    @staticmethod
-    async def create(bot, guild, config: Config, id, action: Literal["edit", "remove"]):
-        return EditFieldsView(bot, config, await config.get(guild), id, action)
-
-    def add_dropdown(self, config_file, id, action):
-        """Add dropdown with options."""
-        options = []
-        for field_id, section in enumerate(config_file [id]["fields"]):
-            options.append(discord.SelectOption(label=f'{id + 1}.{field_id + 1}. {section["name"]}'))
-
-        select = Select(
-            placeholder='Select a field...', 
-            min_values=1, 
-            max_values=1, 
-            options=options
-        )
-
-        if(action == "remove"):
-            select.callback = self.remove_field_callback
-        if(action == "edit"):
-            select.callback = self.edit_field_callback
-
-        self.add_item(select)
-
-    async def edit_field_callback(self, interaction: discord.Interaction):
-        # get id and field id
-        id = int(interaction.data["values"][0].split(".")[0]) - 1
-        field_id = int(interaction.data["values"][0].split(".")[1]) - 1
-
-        modal = FieldModal(title=f"Field editor :pencil:",
-                           config_file=await self.config.get(interaction.guild), section_id=id, field_id=field_id,
-                           bot=self.bot)
-
-        await interaction.response.send_modal(modal)
-
-    async def remove_field_callback(self, interaction: discord.Interaction):
-        # get id and field id
-        id = int(interaction.data["values"][0].split(".")[0]) - 1
-        field_id = int(interaction.data["values"][0].split(".")[1]) - 1
-
-        config_file = await self.config.get(interaction.guild)
-        name = config_file[id]["title"]
-        emoji = config_file[id]["emoji"]
-
-        del config_file[id]["fields"][field_id]
-
-        await self.config.update(interaction.guild, config_file)
-        await interaction.response.edit_message(
-            content=f"Field no. {field_id + 1} has been removed from section {name} {emoji}.", view=None)
-
-class FieldModal(discord.ui.Modal):
-    def __init__(self, bot, config_file, section_id, field_id, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-        self.bot = bot
-        self.config_file = config_file
-        self.section_id = section_id
-        self.field_id = field_id
-
-        name = config_file[section_id]["fields"][field_id]["name"]
-        contents = config_file[section_id]["fields"][field_id]["value"]
-        inline = config_file[section_id]["fields"][field_id]["inline"]
-
-        _title = discord.ui.InputText(
-            label="Title",
-            value=name if name != "" else "Example title 💡",
-            placeholder="Think of an interesting title",
-            max_length=256
-        )
-
-        _contents = discord.ui.InputText(
-            label="Contents",
-            value=contents if contents != "" else "[Example hyperlink](https://github.com/ThatRedKite/thatkitebot)",
-            placeholder="Here you can type the contents of this field in your embed",
-            style=discord.InputTextStyle.long,
-            max_length=1024
-        )
-
-        _inline = discord.ui.InputText(
-            label="In-line",
-            value=inline if inline != "" else "true",
-            placeholder="Type \"true\" or \"false\"",
-            max_length=5
-        )
-
-        self.add_item(_title)
-        self.add_item(_contents)
-        self.add_item(_inline)
-
-    async def callback(self, interaction: discord.Interaction):
-
-        self.config_file[self.section_id]["fields"][self.field_id]["name"] = self.children[0].value  # set title of the field
-        self.config_file[self.section_id]["fields"][self.field_id]["value"] = self.children[1].value  # set value of the field
-
-        # Check if "inline" input value is correct
-        inline_value = str(self.children[2].value).strip().lower()
-
-        if inline_value in {"true", "false"}:
-            self.config_file[self.section_id]["fields"][self.field_id]["inline"] = (inline_value == "true")
-        else:
-            await interaction.response.send_message("Invalid `in-line` value!", ephemeral=True)
-            return
-
-        # update config
-        await self.bot.get_cog("InfoCog").config.update(interaction.guild, self.config_file)
-
-        await interaction.response.send_message(
-            f"Field with ID {self.field_id + 1} in section {self.config_file[self.section_id]['title']} has been changed.")
-        
-
-        
-class FactoryResetModal(discord.ui.Modal):
-    def __init__(self, config, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-        self.config = config
-
-        _decision = discord.ui.InputText(
-            label="Are you sure? Type \"YES\" or \"NO\"",
-            value="",
-            placeholder="It will **replace all of your sections!",
-            max_length=256
-        )
-
-        self.add_item(_decision)
-
-    async def callback(self, interaction: discord.Interaction):
-        """Factory reset all the sections in /info """
-
-        if self.children[0].value == "YES":
-             # load default config
-            default_config = await self.config.get_default()
-
-            # replace the contents of the file with the default values
-            await self.config.update(interaction.guild, default_config)
-
-            await interaction.response.send_message(content="Default settings have been restored.", view=None)
-        else:
-            await interaction.response.send_message(content="Nothing has changed.", view=None)
-
-
 class InfoCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -474,7 +236,251 @@ class InfoCog(commands.Cog):
         if not self.config.exists(guild):
             await self.config.load_default(guild)
 
-    
+
+###### Views ######
+
+class NavigationView(View):
+    def __init__(self, config, config_file, buttons, dropdown, toggle):
+        super().__init__(timeout = None)
+
+        self.config = config 
+        self.config_file = config_file
+
+        # Current states of the view contents
+        self.toggle = toggle
+        self.state_buttons = buttons
+        self.state_dropdown = dropdown
+        self.current_embed_id = None
+
+        if dropdown and not toggle:
+            self.add_dropdown(config_file)
+        if buttons and not toggle:
+            self.add_buttons()
+
+    @staticmethod
+    async def create(guild, config: Config = None, buttons: bool=False, dropdown: bool= True, toggle: bool = False):
+        return NavigationView(config, await config.get(guild), buttons, dropdown, toggle)
+
+    def add_buttons(self):
+        """Add navigation buttons."""
+        prev = Button(emoji="⬅️", style=discord.ButtonStyle.gray, custom_id="prev")
+        next = Button(emoji="➡️", style=discord.ButtonStyle.gray, custom_id="next")
+
+        prev.callback = self.button_callback
+        next.callback = self.button_callback
+
+        self.add_item(prev)
+        self.add_item(next)
+
+    def add_dropdown(self, config_file):
+        """Add dropdown with options."""
+        options = []
+        for id, section in enumerate(config_file):
+            if discord_emoji.to_uni(section["emoji"]):
+                options.append(discord.SelectOption(label=f'{id + 1}. {section["title"]}', emoji=discord_emoji.to_uni(section["emoji"]), value = f"{id}"))
+            else:
+                options.append(discord.SelectOption(label=f'{id + 1}. {section["title"]}', emoji=section["emoji"], value = f"{id}"))
+
+        select = Select(
+            placeholder='Select a section...', 
+            min_values=1, 
+            max_values=1, 
+            options=options
+        )
+
+        select.callback = self.dropdown_callback
+
+        self.add_item(select)
+
+    async def button_callback(self, interaction: discord.Interaction):
+        """Callback function for the next button."""
+        max_id = await self.config.size(interaction.guild)
+
+        if interaction.custom_id == "next":
+            self.current_embed_id = (self.current_embed_id + 1) % max_id
+        else:
+            self.current_embed_id = (self.current_embed_id - 1) % max_id
+
+        config_file = await self.config.get(interaction.guild)
+        embed = await get_embed(self.current_embed_id, config_file)
+
+        await interaction.response.edit_message(embed=embed, content=None, view = self)
+
+
+    async def dropdown_callback(self, interaction: discord.Interaction):
+        """Callback function for the dropdown selection."""
+        self.current_embed_id = int(interaction.data["values"][0])
+        config_file = await self.config.get(interaction.guild)
+
+        embed = await get_embed(self.current_embed_id, config_file)
+
+        if(self.toggle):
+            self.disable_all_items
+        else:
+            if(not self.state_buttons): 
+                self.add_buttons()
+                self.state_buttons = True
+            if(not self.state_dropdown): 
+                self.add_dropdown(config_file)
+                self.state_dropdown =  True
+
+        await interaction.response.edit_message(embed=embed, content=None, view = self)
+
+class EditFieldsView(View):
+    def __init__(self, bot, config, config_file, id ,action):
+        super().__init__(timeout = None)
+
+        self.bot = bot
+        self.config = config 
+        self.config_file = config_file
+
+        self.add_dropdown(config_file, id, action)
+
+    @staticmethod
+    async def create(bot, guild, config: Config, id, action: Literal["edit", "remove"]):
+        return EditFieldsView(bot, config, await config.get(guild), id, action)
+
+    def add_dropdown(self, config_file, id, action):
+        """Add dropdown with options."""
+        options = []
+        for field_id, section in enumerate(config_file [id]["fields"]):
+            options.append(discord.SelectOption(label=f'{id + 1}.{field_id + 1}. {section["name"]}'))
+
+        select = Select(
+            placeholder='Select a field...', 
+            min_values=1, 
+            max_values=1, 
+            options=options
+        )
+
+        if(action == "remove"):
+            select.callback = self.remove_field_callback
+        if(action == "edit"):
+            select.callback = self.edit_field_callback
+
+        self.add_item(select)
+
+    async def edit_field_callback(self, interaction: discord.Interaction):
+        # get id and field id
+        id = int(interaction.data["values"][0].split(".")[0]) - 1
+        field_id = int(interaction.data["values"][0].split(".")[1]) - 1
+
+        modal = FieldModal(title=f"Field editor :pencil:",
+                           config_file=await self.config.get(interaction.guild), section_id=id, field_id=field_id,
+                           bot=self.bot)
+
+        await interaction.response.send_modal(modal)
+
+    async def remove_field_callback(self, interaction: discord.Interaction):
+        # get id and field id
+        id = int(interaction.data["values"][0].split(".")[0]) - 1
+        field_id = int(interaction.data["values"][0].split(".")[1]) - 1
+
+        config_file = await self.config.get(interaction.guild)
+        name = config_file[id]["title"]
+        emoji = config_file[id]["emoji"]
+
+        del config_file[id]["fields"][field_id]
+
+        await self.config.update(interaction.guild, config_file)
+        await interaction.response.edit_message(
+            content=f"Field no. {field_id + 1} has been removed from section {name} {emoji}.", view=None)
+
+
+###### Modals ######
+
+class FieldModal(discord.ui.Modal):
+    def __init__(self, bot, config_file, section_id, field_id, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.bot = bot
+        self.config_file = config_file
+        self.section_id = section_id
+        self.field_id = field_id
+
+        name = config_file[section_id]["fields"][field_id]["name"]
+        contents = config_file[section_id]["fields"][field_id]["value"]
+        inline = config_file[section_id]["fields"][field_id]["inline"]
+
+        _title = discord.ui.InputText(
+            label="Title",
+            value=name if name != "" else "Example title 💡",
+            placeholder="Think of an interesting title",
+            max_length=256
+        )
+
+        _contents = discord.ui.InputText(
+            label="Contents",
+            value=contents if contents != "" else "[Example hyperlink](https://github.com/ThatRedKite/thatkitebot)",
+            placeholder="Here you can type the contents of this field in your embed",
+            style=discord.InputTextStyle.long,
+            max_length=1024
+        )
+
+        _inline = discord.ui.InputText(
+            label="In-line",
+            value=inline if inline != "" else "true",
+            placeholder="Type \"true\" or \"false\"",
+            max_length=5
+        )
+
+        self.add_item(_title)
+        self.add_item(_contents)
+        self.add_item(_inline)
+
+    async def callback(self, interaction: discord.Interaction):
+
+        self.config_file[self.section_id]["fields"][self.field_id]["name"] = self.children[0].value  # set title of the field
+        self.config_file[self.section_id]["fields"][self.field_id]["value"] = self.children[1].value  # set value of the field
+
+        # Check if "inline" input value is correct
+        inline_value = str(self.children[2].value).strip().lower()
+
+        if inline_value in {"true", "false"}:
+            self.config_file[self.section_id]["fields"][self.field_id]["inline"] = (inline_value == "true")
+        else:
+            await interaction.response.send_message("Invalid `in-line` value!", ephemeral=True)
+            return
+
+        # update config
+        await self.bot.get_cog("InfoCog").config.update(interaction.guild, self.config_file)
+
+        await interaction.response.send_message(
+            f"Field with ID {self.field_id + 1} in section {self.config_file[self.section_id]['title']} has been changed.")
+        
+
+        
+class FactoryResetModal(discord.ui.Modal):
+    def __init__(self, config, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.config = config
+
+        _decision = discord.ui.InputText(
+            label="Are you sure? Type \"YES\" or \"NO\"",
+            placeholder="It will **replace all of your sections!",
+            max_length=3
+        )
+
+        self.add_item(_decision)
+
+    async def callback(self, interaction: discord.Interaction):
+        """Factory reset all the sections in /info """
+
+        if self.children[0].value == "YES":
+             # load default config
+            default_config = await self.config.get_default()
+
+            # replace the contents of the file with the default values
+            await self.config.update(interaction.guild, default_config)
+
+            await interaction.response.send_message(content="Default settings have been restored.", view=None)
+        else:
+            await interaction.response.send_message(content="Nothing has changed.", view=None)
+
+
+###### Utility ######
+
 class Utility:
     # generate random 24 bit number
     @staticmethod
