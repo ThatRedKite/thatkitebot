345b50b48bd6da7514fd2b02572b873249aedb80
ThatRedKite <thatredkite@thatredkite.de>
2026-04-12 13:07:39 +0200

    give redis some time to load

--- thatkitebot/__main__.py
+++ thatkitebot/__main__.py
@@ -35,6 +35,7 @@ from discord.abc import GuildChannel, PrivateChannel
 from abc import ABC
 from datetime import datetime
 from pathlib import Path
+from time import sleep
 
 import aiohttp
 import psutil
@@ -167,11 +168,18 @@ class ThatKiteBot(commands.Bot, ABC):
             # db 0 is free at the moment as it formerly contained the auth stuff
             # this initializes all the different redis connections needed to run kitebot
             self.redis = aioredis.Redis(connection_pool=conn_pool, db=1, decode_responses=True)
+
             self.redis_repost = aioredis.Redis(connection_pool=conn_pool, db=2, decode_responses=True)
             self.redis_welcomes = aioredis.Redis(connection_pool=conn_pool, db=3, decode_responses=True)
             self.redis_bookmarks = aioredis.Redis(connection_pool=conn_pool, db=4, decode_responses=True)
             self.redis_starboard = aioredis.Redis(connection_pool=conn_pool, db=5, decode_responses=True)
             self.persistent_cache = aioredis.Redis(connection_pool=conn_pool, db=6, decode_responses=False)
+
+            for _ in range(0,9):
+                try:
+                    self.redis.ping()
+                except aioredis.BusyLoadingError:
+                    sleep(1)
             
             # initialize the async and the normal cache classes
             self.r_cache = RedisCacheAsync(self, auto_exec=False, host=self.redis_host_cache, persistent_host=self.redis_host)
